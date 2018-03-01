04c42dd95b8487ca88f521ce24ccf19453dfa3c4
Shirley Lin <shirley951025@gmail.com>
2018-03-01 13:52:44 +0800

    twinkle led

--- sound/sound.ino
+++ sound/sound.ino
@@ -5,6 +5,12 @@ int ledPin2=2;
 int ledPin3=3;
 int ledPin4=4;
 int ledPin5=5;
+int smallLedPin1=A1;
+int smallLedPin2=A2;
+int smallLedPin3=A3;
+int smallLedPin4=A4;
+char onLed=HIGH;
+char offLed=LOW;
 
 void setup(){
   pinMode(soundPin, INPUT);
@@ -13,14 +19,14 @@ void setup(){
   pinMode(ledPin3, OUTPUT);
   pinMode(ledPin4, OUTPUT);
   pinMode(ledPin5, OUTPUT);
+  pinMode(smallLedPin1, OUTPUT);
+  pinMode(smallLedPin2, OUTPUT);
+  pinMode(smallLedPin3, OUTPUT);
+  pinMode(smallLedPin4, OUTPUT);
   Serial.begin(9600);
 }
 void loop(){
-  digitalWrite(ledPin1, LOW);
-  digitalWrite(ledPin2, LOW);
-  digitalWrite(ledPin3, LOW);
-  digitalWrite(ledPin4, LOW);
-  digitalWrite(ledPin5, LOW);
+  twinkleLed();
   int val = analogRead(soundPin);
   if(val>50){
     totalSound+=val;
@@ -66,5 +72,26 @@ void loop(){
   digitalWrite(ledPin5, HIGH);
   Serial.println(totalSound);
   }
-  
 }
+void twinkleLed(){
+    int delaySec=100;
+    digitalWrite(smallLedPin1, onLed);
+    digitalWrite(smallLedPin2, offLed);
+    digitalWrite(smallLedPin3, offLed);
+    digitalWrite(smallLedPin4, offLed);
+    delay(delaySec);
+    digitalWrite(smallLedPin2, onLed);
+    delay(delaySec);
+    digitalWrite(smallLedPin3, onLed);
+    delay(delaySec);
+    digitalWrite(smallLedPin4, onLed);
+    delay(delaySec);
+    digitalWrite(smallLedPin4, offLed);
+    delay(delaySec);
+    digitalWrite(smallLedPin3, offLed);
+    delay(delaySec);
+    digitalWrite(smallLedPin2, offLed);
+    delay(delaySec);
+    digitalWrite(smallLedPin1, offLed);
+    delay(delaySec);
+  }
