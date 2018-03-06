85e298763eae4a40309b11ae82356901626963db
Shirley Lin <shirley951025@gmail.com>
2018-03-06 15:04:29 +0800

    default value

--- sound/sound.ino
+++ sound/sound.ino
@@ -1,10 +1,19 @@
 int soundPin=A0;
 int totalSound=0;
-int ledPin1=6;
+int ledPin1=1;
 int ledPin2=2;
 int ledPin3=3;
 int ledPin4=4;
 int ledPin5=5;
+int ledPin6=6;
+int ledPin7=7;
+int ledPin8=8;
+int ledPin9=9;
+int ledPin10=10;
+int ledPin11=11;
+int ledPin12=12;
+int ledPin13=10;
+int ledPin14=A5;
 int smallLedPin1=A1;
 int smallLedPin2=A2;
 int smallLedPin3=A3;
@@ -14,12 +23,12 @@ char onLed=HIGH;
 char offLed=LOW;
 
 void setup(){
+  
   pinMode(soundPin, INPUT);
-  pinMode(ledPin1, OUTPUT);
-  pinMode(ledPin2, OUTPUT);
-  pinMode(ledPin3, OUTPUT);
-  pinMode(ledPin4, OUTPUT);
-  pinMode(ledPin5, OUTPUT);
+  for(int i=1; i<=13 ;i++){
+    pinMode(i, OUTPUT);
+    }
+  pinMode(ledPin14, OUTPUT);
   pinMode(smallLedPin1, OUTPUT);
   pinMode(smallLedPin2, OUTPUT);
   pinMode(smallLedPin3, OUTPUT);
@@ -65,49 +74,125 @@ void twinkleLed(){
     Serial.println(val);
     if(val>48 && storeLed == true){
     totalSound+=val;
-    digitalWrite(7, HIGH);
-    delay(500);
-    digitalWrite(7, LOW);
+    //success add
     Serial.println(val);
   }
-  if(totalSound>=100 && totalSound<200){
-  digitalWrite(ledPin1, HIGH);
-  digitalWrite(ledPin2, LOW);
-  digitalWrite(ledPin3, LOW);
-  digitalWrite(ledPin4, LOW);
-  digitalWrite(ledPin5, LOW);
+  int sound=100;
+  if(totalSound>=sound && totalSound<sound+200){
+  for(int i=1; i<2; i++){
+  digitalWrite(i, HIGH);
+  }
+  for(int i=2; i<13; i++){
+  digitalWrite(i, LOW);
+  }
+  digitalWrite(ledPin14, LOW);
   Serial.println(totalSound);
   }
-  else if(totalSound>=200 && totalSound<400){
-  digitalWrite(ledPin1, HIGH);
-  digitalWrite(ledPin2, HIGH);
-  digitalWrite(ledPin3, LOW);
-  digitalWrite(ledPin4, LOW);
-  digitalWrite(ledPin5, LOW);
+  else if(totalSound>=sound+200 && totalSound<sound+400){
+  for(int i=1; i<3; i++){
+  digitalWrite(i, HIGH);
+  }
+  for(int i=3; i<13; i++){
+  digitalWrite(i, LOW);
+  }
+  digitalWrite(ledPin14, LOW);
   Serial.println(totalSound);
   }
-  else if(totalSound>=400 && totalSound<600){
-  digitalWrite(ledPin1, HIGH);
-  digitalWrite(ledPin2, HIGH);
-  digitalWrite(ledPin3, HIGH);
-  digitalWrite(ledPin4, LOW);
-  digitalWrite(ledPin5, LOW);
+  else if(totalSound>=sound+400 && totalSound<sound+600){
+ for(int i=1; i<4; i++){
+  digitalWrite(i, HIGH);
+  }
+  for(int i=4; i<13; i++){
+  digitalWrite(i, LOW);
+  }
+  digitalWrite(ledPin14, LOW);
   Serial.println(totalSound);
   }
-  else if(totalSound>=600 && totalSound<800){
-  digitalWrite(ledPin1, HIGH);
-  digitalWrite(ledPin2, HIGH);
-  digitalWrite(ledPin3, HIGH);
-  digitalWrite(ledPin4, HIGH);
-  digitalWrite(ledPin5, LOW);
+  else if(totalSound>=sound+600 && totalSound<sound+800){
+  for(int i=1; i<5; i++){
+  digitalWrite(i, HIGH);
+  }
+  for(int i=5; i<13; i++){
+  digitalWrite(i, LOW);
+  }
+  digitalWrite(ledPin14, LOW);
   Serial.println(totalSound);
   }
-  else if(totalSound>=800){
-  digitalWrite(ledPin1, HIGH);
-  digitalWrite(ledPin2, HIGH);
-  digitalWrite(ledPin3, HIGH);
-  digitalWrite(ledPin4, HIGH);
-  digitalWrite(ledPin5, HIGH);
+  else if(totalSound>=sound+800 && totalSound<sound+1000){
+  for(int i=1; i<6; i++){
+  digitalWrite(i, HIGH);
+  }
+  for(int i=6; i<13; i++){
+  digitalWrite(i, LOW);
+  }
+  digitalWrite(ledPin14, LOW);
+  Serial.println(totalSound);
+  }
+  else if(totalSound>=sound+1000 && totalSound<sound+1200){
+  for(int i=1; i<7; i++){
+  digitalWrite(i, HIGH);
+  }
+  for(int i=7; i<13; i++){
+  digitalWrite(i, LOW);
+  }
+  digitalWrite(ledPin14, LOW);
+  Serial.println(totalSound);
+  }
+  else if(totalSound>=sound+1200 && totalSound<sound+1400){
+  for(int i=1; i<8; i++){
+  digitalWrite(i, HIGH);
+  }
+  for(int i=8; i<13; i++){
+  digitalWrite(i, LOW);
+  }
+  digitalWrite(ledPin14, LOW);
+  Serial.println(totalSound);
+  }
+  else if(totalSound>=sound+1400 && totalSound<sound+1600){
+  for(int i=1; i<9; i++){
+  digitalWrite(i, HIGH);
+  }
+  for(int i=9; i<13; i++){
+  digitalWrite(i, LOW);
+  }
+  digitalWrite(ledPin14, LOW);
+  Serial.println(totalSound);
+  }
+  else if(totalSound>=sound+1600 && totalSound<sound+1800){
+  for(int i=1; i<10; i++){
+  digitalWrite(i, HIGH);
+  }
+  for(int i=10; i<13; i++){
+  digitalWrite(i, LOW);
+  }
+  digitalWrite(ledPin14, LOW);
+  Serial.println(totalSound);
+  }
+  else if(totalSound>=sound+1800 && totalSound<sound+2000){
+  for(int i=1; i<11; i++){
+  digitalWrite(i, HIGH);
+  }
+  for(int i=11; i<13; i++){
+  digitalWrite(i, LOW);
+  }
+  digitalWrite(ledPin14, LOW);
+  Serial.println(totalSound);
+  }
+  else if(totalSound>=sound+2000 && totalSound<sound+2200){
+  for(int i=1; i<12; i++){
+  digitalWrite(i, HIGH);
+  }
+  for(int i=12; i<13; i++){
+  digitalWrite(i, LOW);
+  }
+  digitalWrite(ledPin14, LOW);
+  Serial.println(totalSound);
+  }
+  else if(totalSound>=2200){
+  for(int i=1; i<14; i++){
+  digitalWrite(i, HIGH);
+  }
+  digitalWrite(ledPin14, HIGH);
   Serial.println(totalSound);
   }
   }
