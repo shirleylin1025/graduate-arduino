7a80eacb09d376c08ef941f8c3b0edf70ecc9b31
Shirley Lin <shirley951025@gmail.com>
2018-03-13 18:18:49 +0800

    test "1"

--- mergeLed/mergeLed.ino
+++ mergeLed/mergeLed.ino
@@ -1,4 +1,10 @@
 #include <SoftwareSerial.h>
+#include "Arduino.h"
+#include "DFRobotDFPlayerMini.h"
+
+SoftwareSerial mySoftwareSerial(A3, A4); // RX, TX
+DFRobotDFPlayerMini myDFPlayer;
+void printDetail(uint8_t type, int value);
 
 SoftwareSerial BT(A1, A2); // 接收腳, 傳送腳
 char val;  // 儲存接收資料的變數
@@ -25,22 +31,58 @@ void setup() {
     pinMode(i, OUTPUT);
     }
   pinMode(ledPin14, OUTPUT);
-  pinMode(A3,OUTPUT);
-  BT.begin(9600);
+  
   Serial.begin(9600);
+  
+  mySoftwareSerial.begin(9600);
+  
+  Serial.println();
+  Serial.println(F("DFRobot DFPlayer Mini Demo"));
+  Serial.println(F("Initializing DFPlayer ... (May take 3~5 seconds)"));
+  
+  if (!myDFPlayer.begin(mySoftwareSerial)) {  //Use softwareSerial to communicate with mp3.
+    Serial.println(F("Unable to begin:"));
+    Serial.println(F("1.Please recheck the connection!"));
+    Serial.println(F("2.Please insert the SD card!"));
+    while(true);
+  }
+  Serial.println(F("DFPlayer Mini online."));
+  
+  myDFPlayer.volume(30);  //Set volume value. From 0 to 30
+  //myDFPlayer.play(2);  //Play the first mp3
 
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
   setLed();
-
+  static unsigned long timer = millis();
+  char val = Serial.read();
+  if (val== '1') {
+    myDFPlayer.play(3);
+    //timer = millis();
+    //myDFPlayer.next();  //Play next mp3 every 3 second.
+  }
+  if (val== '0') {
+    myDFPlayer.play(2);
+    //timer = millis();
+    //myDFPlayer.next();  //Play next mp3 every 3 second.
+  }
+  
+  if (myDFPlayer.available()) {
+    printDetail(myDFPlayer.readType(), myDFPlayer.read()); //Print the detail message from DFPlayer to handle different errors and states.
+  }
+  //static unsigned long timer = millis();
+  BT.begin(9600);
   if (BT.available()) {
     val = BT.read();
+    Serial.println(val);
+    timer = millis();
+    myDFPlayer.play(3);
     if (val == '1') {
-      digitalWrite(A3, HIGH);
-      // 回應命令發送端，告知「已開燈」
+      Serial.println(val);
       BT.println("LED ON");
+      myDFPlayer.play(3);
     } else if (val == '0') {
       digitalWrite(A3, LOW);
       // 回應命令發送端，告知「已關燈」
@@ -170,3 +212,69 @@ void setLed(){
   Serial.println(totalSound);
   }
   }
+void playPlayer(){
+  static unsigned long timer = millis();
+  
+  if (millis() - timer > 5000) {
+    timer = millis();
+    myDFPlayer.next();  //Play next mp3 every 3 second.
+  }
+  
+  if (myDFPlayer.available()) {
+    printDetail(myDFPlayer.readType(), myDFPlayer.read()); //Print the detail message from DFPlayer to handle different errors and states.
+  }
+  }
+void printDetail(uint8_t type, int value){
+  switch (type) {
+    case TimeOut:
+      Serial.println(F("Time Out!"));
+      break;
+    case WrongStack:
+      Serial.println(F("Stack Wrong!"));
+      break;
+    case DFPlayerCardInserted:
+      Serial.println(F("Card Inserted!"));
+      break;
+    case DFPlayerCardRemoved:
+      Serial.println(F("Card Removed!"));
+      break;
+    case DFPlayerCardOnline:
+      Serial.println(F("Card Online!"));
+      break;
+    case DFPlayerPlayFinished:
+      Serial.print(F("Number:"));
+      Serial.print(value);
+      Serial.println(F(" Play Finished!"));
+      break;
+    case DFPlayerError:
+      Serial.print(F("DFPlayerError:"));
+      switch (value) {
+        case Busy:
+          Serial.println(F("Card not found"));
+          break;
+        case Sleeping:
+          Serial.println(F("Sleeping"));
+          break;
+        case SerialWrongStack:
+          Serial.println(F("Get Wrong Stack"));
+          break;
+        case CheckSumNotMatch:
+          Serial.println(F("Check Sum Not Match"));
+          break;
+        case FileIndexOut:
+          Serial.println(F("File Index Out of Bound"));
+          break;
+        case FileMismatch:
+          Serial.println(F("Cannot Find File"));
+          break;
+        case Advertise:
+          Serial.println(F("In Advertise"));
+          break;
+        default:
+          break;
+      }
+      break;
+    default:
+      break;
+  }
+}
