96c1f1edf8448cd665707414aad22a574102831b
Shirley Lin <shirley951025@gmail.com>
2018-03-29 14:12:04 +0800

    commit

--- player/player.ino
+++ player/player.ino
@@ -2,7 +2,7 @@
 #include "SoftwareSerial.h"
 #include "DFRobotDFPlayerMini.h"
 
-SoftwareSerial mySoftwareSerial(10, 11); // RX, TX
+SoftwareSerial mySoftwareSerial(4, 5); // RX, TX
 DFRobotDFPlayerMini myDFPlayer;
 void printDetail(uint8_t type, int value);
 
@@ -24,7 +24,7 @@ void setup()
   Serial.println(F("DFPlayer Mini online."));
   
   myDFPlayer.volume(30);  //Set volume value. From 0 to 30
-  myDFPlayer.play(2);  //Play the first mp3
+  myDFPlayer.play(1);  //Play the first mp3
 }
 
 void loop()
